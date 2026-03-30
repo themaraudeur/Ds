1a4fc3ada0808f292b4189b98185349e40fa58a8
themaraudeur <kyllianmollier64@gmail.com>
2026-03-30 18:08:34 +0200

    Add files via upload

---
+++ Www/assets/emulatorjs/data.js
@@ -0,0 +1,28 @@
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404: Page not found</title>
+    <style>
+      body, html {
+        background-color: #000;
+      }
+      h2, p {
+        color: #fff;
+      }
+      a {
+        color: #0000EE;
+        text-decoration: underline;
+        cursor: pointer;
+      }
+      a:visited {
+        color: #800080;
+      }
+    </style>
+  </head>
+  <body>
+    <h2>404: Page not found</h2>
+    <p>Idk where you're trying to go, but its not here....</p>
+    <a herf="#" onclick="history.back()">Go Back?</a>
+  <script defer src="https://static.cloudflareinsights.com/beacon.min.js/v8c78df7c7c0f484497ecbca7046644da1771523124516" integrity="sha512-8DS7rgIrAmghBFwoOTujcf6D9rXvH8xm8JQ1Ja01h9QX8EzXldiszufYa4IFfKdLUKTTrnSFXLDkUEOTrZQ8Qg==" data-cf-beacon='{"version":"2024.11.0","token":"192fdaadd1094fdfa2eb6daf77e04988","r":1,"server_timing":{"name":{"cfCacheStatus":true,"cfEdge":true,"cfExtPri":true,"cfL4":true,"cfOrigin":true,"cfSpeedBrain":true},"location_startswith":null}}' crossorigin="anonymous"></script>
+</body>
+</html>
